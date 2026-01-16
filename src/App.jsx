c2feea94b4516fd8b313bc6ac73bd632719fa0f7
amitpatelAP <amitpatelAP@users.noreply.github.com>
2026-01-17 01:42:46 +0530

    Enhance component reusability

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Newsletter from './components/Newsletter';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ShopProduct from './pages/ShopProduct';
+import Blog from './pages/Blog';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+
+const AppLayout = () => {
+    const location = useLocation();
+    const showNewsletter = location.pathname !== '/cart';
+
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/shop-product" element={<ShopProduct />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+            {showNewsletter && <Newsletter />}
+            <Footer />
+        </>
+    );
+};
+
+function App() {
+    return (
+        <Router>
+            <AppLayout />
+        </Router>
+    );
+}
+
+export default App;
